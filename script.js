03d2d80d171253e6feb30a9a18b88b552f1b147c
ayushgandhi2212-stack <ayushgandhi2212@gmail.com>
2026-03-26 14:12:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showMessage() {
+  alert("Thanks for visiting my portfolio!");
+}
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
